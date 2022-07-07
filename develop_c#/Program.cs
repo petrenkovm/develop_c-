a94a864c7eae1155d72efa2ee506ede4612bc17b
pvm <petrenkovm@hotmail.com>
2022-07-07 20:58:10 +0300

    created magic

--- develop_c#/Program.cs
+++ develop_c#/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Vitalii!");
+﻿Console.Clear();
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("*");
+
+int x = xa, y = ya;
+int count = 0;
+
+while(count<50000) 
+{
+    int rand = new Random().Next(0,3);
+
+    if(rand == 0)
+    {
+        x = (x + xa)/2;
+        y = (y + ya)/2;
+    }
+    if(rand == 1)
+    {
+        x = (x + xb)/2;
+        y = (y + yb)/2;
+    }
+    if(rand == 2)
+    {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("*");
+    count++;
+}
